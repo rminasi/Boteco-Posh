b4084dec3804afc9ce6ea87fc646cb8833e8a1b9
Raquel Minasi <raquelminasi@gmail.com>
2019-09-01 21:34:14 +0100

    some styling applied

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -15,17 +15,21 @@ class CocktailsController < ApplicationController
   def create
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
-      redirect_to cocktail_path(@cocktail)
+      redirect_to cocktails_path(@cocktail)
     else
       render :new
     end
   end
 
+  # def update
+  #   @cocktail = Cocktail.find(params[:id])
+  #   @cocktail.update
+  # end
+
   def destroy
     @cocktail = Cocktail.find(params[:id])
     @cocktail.destroy
     redirect_to cocktails_path
-
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,9 @@ Rails.application.routes.draw do
   # get 'cocktails/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
-  resources :cocktails, only: [:index, :show, :new, :create] do
+  resources :cocktails do
     resources :doses, only: [:create]
   end
   resources :doses, only: [:destroy]
+  delete 'cocktail/:id', to: 'cocktails#destroy', as: :delete_cocktail
 end

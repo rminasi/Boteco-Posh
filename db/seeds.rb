88c1a5dcb76251434fc30d0d69c979897754acae
Raquel Minasi <raquelminasi@gmail.com>
2019-09-08 00:50:06 +0100

    navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-require 'open-uri'
 require 'json'
+require 'open-uri'
 
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredients_serialized = open(url).read
 ingredients = JSON.parse(ingredients_serialized)
 drink_list = ingredients['drinks']
 drink_list.each do |drink|
-  Ingredient.create(name: drink['strIngredient1'])
+  Ingredient.create(name: "#{drink['strIngredient1']}")
 end

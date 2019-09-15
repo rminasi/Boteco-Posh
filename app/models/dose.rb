59eed329c2de63a5ad0c4d8834a4c039def2f5e3
Raquel Minasi <raquelminasi@gmail.com>
2019-09-15 22:16:21 +0100

    seed.rb amended

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,6 +2,6 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
 
-  validates :description, presence: true, allow_blank: false
+  validates :description, presence: true, allow_blank: true
   validates :cocktail, uniqueness: { scope: :ingredient_id }
 end
